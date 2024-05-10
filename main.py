4f1827c245f47088bc397b2b80e138ce752067fe
Khalil <khalil.j.khalil2@gmail.com>
2024-05-10 13:03:15 -0400

    Added WIDTH, HEIGHT, And The Name For The Window

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import pygame
 import math
 
 pygame.init()
+
+#Sets The Width, Height, And Name Of The Window
+WIDTH, HEIGHT = 600, 400
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Gravitational Slingshot')
