c4ceea5e047830aa793992b437638ead904e6585
Khalil <khalil.j.khalil2@gmail.com>
2024-05-10 13:12:38 -0400

    Added Mass/Size Of Planet And Ship And The Gravity. Set The FPS For The Window

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ pygame.init()
 WIDTH, HEIGHT = 600, 400
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Gravitational Slingshot')
+
+#Mass Of The Planet And Ship(Obj) As Well As The Gravity. Affects The Slingshot Effect
+PLANET_MASS = 100
+SHIP_MASS = 5
+G = 5
+
+#FPS The Window Runs At
+FPS = 60
+
+#Size Of The Planet And Our Ship(Obj)
+PLANET_SIZE = 50
+OBJ_SIZE = 5
+
+VEL_SCALE = 100
